6efb9ad2284614d701a7663f4a696f665baedf97
kainlan <cjkolek@gmail.com>
2022-05-30 13:23:52 -0400

    AC4 Decoding SUpport

--- libavcodec/codec_id.h
+++ libavcodec/codec_id.h
@@ -519,6 +519,7 @@ enum AVCodecID {
     AV_CODEC_ID_FASTAUDIO,
     AV_CODEC_ID_MSNSIREN,
     AV_CODEC_ID_DFPWM,
+	AV_CODEC_ID_AC4,
 
     /* subtitle codecs */
     AV_CODEC_ID_FIRST_SUBTITLE = 0x17000,          ///< A dummy ID pointing at the start of subtitle codecs.
